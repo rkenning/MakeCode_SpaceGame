0eea137cb814422ad7574faa762664836339aa54
Ryan Kenning <rkenning2@yahoo.co.uk>
2023-09-10 10:17:07 +0100

    Ship changes

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
-    
     projectile = sprites.createProjectileFromSprite(img`
             . . . . . . . . 
                     . . . . . . . . 
@@ -12,19 +11,23 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
         `, ship, 0, -140)
     projectile.startEffect(effects.coolRadial, 100)
 })
+
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_on_overlap(sprite2: Sprite, otherSprite2: Sprite) {
     scene.cameraShake(4, 500)
     otherSprite2.destroy(effects.disintegrate)
     sprite2.startEffect(effects.fire, 200)
     info.changeLifeBy(-1)
 })
+
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_on_overlap2(sprite: Sprite, otherSprite: Sprite) {
     sprite.destroy()
     otherSprite.destroy(effects.disintegrate)
     info.changeScoreBy(1)
 })
-let projectile : Sprite = null
-let ship : Sprite = null
+
+
+let projectile: Sprite = null
+let ship: Sprite = null
 let asteroids = [sprites.space.spaceSmallAsteroid1, sprites.space.spaceSmallAsteroid0, sprites.space.spaceAsteroid0, sprites.space.spaceAsteroid1, sprites.space.spaceAsteroid4, sprites.space.spaceAsteroid3]
 ship = sprites.create(assets.image`ShipRight`, SpriteKind.Player)
 
@@ -33,10 +36,11 @@ ship.bottom = 120
 controller.moveSprite(ship, 100, 100)
 info.setLife(3)
 effects.starField.startScreenEffect()
+
+
 game.onUpdateInterval(500, function on_update_interval() {
-    
-    projectile = sprites.createProjectileFromSide(asteroids[randint(0, asteroids.length - 1)], 0, 75)
+    projectile = sprites.createProjectileFromSide(asteroids[randint(0, asteroids.length - 1)], 0, 30)
     projectile.setKind(SpriteKind.Enemy)
     projectile.x = randint(10, 150)
-    animation.runImageAnimation(ship,assets.animation`ShipAnim`, 50)
+    animation.runImageAnimation(ship, assets.animation`ShipAnim`, 50)
 })
